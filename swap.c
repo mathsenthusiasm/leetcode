661a2d47dd39c2e5d99a215e4fed477a67263962
Anish kumar singh <anishkumarsingh1234517@gmail.com>
2026-05-16 09:03:28 +0530

    Correct swap function in swap.c
    Fix syntax errors and implement swapping logic.

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+  int n,m
+printf("enter any number:);
+  scanf("%d",&n);
+printf("enter any number:");
+scanf("%d",&m);
+//before swapping
+printf("n=%d,m=%d\n",n,m);
+int temp=n
+n=m;
+m=n;
+  //after swapping
+printf("n=%d,m=%d\n",n,m);
+}
+
